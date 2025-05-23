edf98d5d6a291b6871df7833af41f9fa84fe46e1
Developer-Madhu <madhuatomix@gmail.com>
2025-05-23 19:01:39 +0530

    added parameters for quantum functions

--- helloibm.py
+++ helloibm.py
@@ -4,7 +4,10 @@ from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
 circuit = QuantumCircuit(2)
 circuit.measure_all()
 
-service = QiskitRuntimeService()
+service = QiskitRuntimeService(
+    token='', #your_api_token_key
+    channel='ibm_quantum' #your_channel_name
+)
 backend = service.least_busy(operational=True, simulator=False)
 
 sampler = Sampler(backend)
