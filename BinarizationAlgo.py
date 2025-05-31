f034ed12e6640d2697a1b539ab6cdaab43c7e6c9
Developer-Madhu <madhuatomix@gmail.com>
2025-05-31 10:57:42 +0530

    seperated functions for processing & printing elements

--- BinarizationAlgo.py
+++ BinarizationAlgo.py
@@ -1,12 +1,17 @@
-gray_img = [[0,3],[5,2]]
-res = []
+grey_img = [[0,3],[5,2]]
+res_img = []
 
-def displayElems(mat1, mat2):
+def processElems(mat1, mat2):
     for rows in mat1:
         tempRows = []
         for elems in rows:
             tempRows.append(f"{elems:08b}")
         mat2.append(tempRows)
 
-displayElems(gray_img, res)
-print(res)
+def displayValuesOfMatrix(mat):
+    for i in mat:
+        for j in i:
+            print(j)
+
+processElems(grey_img, res_img)
+displayValuesOfMatrix(res_img)
